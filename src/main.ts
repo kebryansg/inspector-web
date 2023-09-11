c1c0a994b94cc0ea747f7a70c1c0555db641b435
KEVIN SUAREZ <kebryansg@gmail.com>
2023-09-11 16:06:38 -0500

    feat: provideHttpClient

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,16 @@ import {AppComponent} from "./app/app.component";
 import {provideRouter} from "@angular/router";
 import {routes} from "./app/app-routing";
 import {importProvidersFrom} from "@angular/core";
-import {HttpClientModule, provideHttpClient, withInterceptors} from "@angular/common/http";
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {TokenInterceptor} from "./app/feature/admin/interceptors/token.interceptor";
 import {DEFAULT_DIALOG_CONFIG, DialogModule} from '@angular/cdk/dialog'
-import {ModalContainerComponent} from "./app/shared/components/modal-container/modal-container.component";
+import {ModalContainerComponent} from "@standalone-shared/modal-container/modal-container.component";
 
 
 bootstrapApplication(AppComponent, {
     providers: [
         importProvidersFrom([BrowserAnimationsModule]),
-        importProvidersFrom(HttpClientModule),
         provideHttpClient(
             withInterceptors([TokenInterceptor])
         ),
