061b4db34a5971ea37b6a59ef67287ac3b4aba37
KEVIN SUAREZ <kebryansg@gmail.com>
2023-09-16 13:23:56 -0500

    feat: devextreme config language es

---
+++ src/app/app.config.ts
@@ -0,0 +1,42 @@
+import {APP_INITIALIZER, ApplicationConfig, importProvidersFrom} from "@angular/core";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
+import {TokenInterceptor} from "./feature/admin/interceptors/token.interceptor";
+import {provideRouter} from "@angular/router";
+import {routes} from "./app-routing";
+import {DEFAULT_DIALOG_CONFIG, DialogModule} from "@angular/cdk/dialog";
+import {ModalContainerComponent} from "@standalone-shared/modal-container/modal-container.component";
+
+// DevExtreme
+import config from 'devextreme/core/config';
+import {loadMessages, locale} from 'devextreme/localization';
+// @ts-ignore
+import esMessages from 'devextreme/localization/messages/es.json';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    importProvidersFrom([BrowserAnimationsModule]),
+    provideHttpClient(
+      withInterceptors([TokenInterceptor])
+    ),
+    provideRouter(routes),
+    importProvidersFrom([
+      DialogModule
+    ]),
+    {
+      provide: DEFAULT_DIALOG_CONFIG,
+      useValue: {
+        hasBackdrop: false,
+        container: ModalContainerComponent,
+      }
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => {
+        loadMessages(esMessages);
+        locale(navigator.language);
+        config({defaultCurrency: 'USD'});
+      }
+    }
+  ]
+}
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,7 @@
 import {bootstrapApplication} from "@angular/platform-browser";
 import {AppComponent} from "./app/app.component";
-import {provideRouter} from "@angular/router";
-import {routes} from "./app/app-routing";
-import {importProvidersFrom} from "@angular/core";
-import {provideHttpClient, withInterceptors} from "@angular/common/http";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {TokenInterceptor} from "./app/feature/admin/interceptors/token.interceptor";
-import {DEFAULT_DIALOG_CONFIG, DialogModule} from '@angular/cdk/dialog'
-import {ModalContainerComponent} from "@standalone-shared/modal-container/modal-container.component";
+import {appConfig} from "./app/app.config";
 
 
-bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom([BrowserAnimationsModule]),
-        provideHttpClient(
-            withInterceptors([TokenInterceptor])
-        ),
-        provideRouter(routes),
-        importProvidersFrom([
-            DialogModule
-        ]),
-        {
-            provide: DEFAULT_DIALOG_CONFIG,
-            useValue: {
-                hasBackdrop: false,
-                container: ModalContainerComponent,
-            }
-        },
-    ]
-});
+bootstrapApplication(AppComponent, appConfig)
+  .catch(e => console.log(e));
