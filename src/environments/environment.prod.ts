706a2b2572f061c26a47e8cf6da74629a4b8210e
KEVIN SUAREZ <kebryansg@gmail.com>
2023-11-21 23:52:17 -0500

    chore: config angular production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import {EnvironmentInt} from "../app/interfaces/environment.interface";
 export const environment: EnvironmentInt = {
   production: true,
   apiUrl: 'https://inspector-backend.onrender.com/',
-  ambiente: 'DEVELOPMENT',
+  ambiente: 'PRODUCTION',
   googleMapsKey: 'AIzaSyBCMLN-ZbmFDw7Rf_OLsCDrYzI4n-jcaX0',
   apiUrlAnexos: 'http://192.168.10.28/',
 };
