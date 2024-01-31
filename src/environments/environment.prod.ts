eb67d7b812a72796f6c7d51a4b48bba2051d3cb8
KEVIN SUAREZ <kebryansg@gmail.com>
2024-01-31 08:25:18 -0500

    chore(api): update api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,6 @@ import {EnvironmentInt} from "../app/interfaces/environment.interface";
 
 export const environment: EnvironmentInt = {
   production: true,
-  apiUrl: 'https://inspector-backend.onrender.com/',
-  ambiente: 'PRODUCTION',
+  apiUrl: 'https://p01--inspector-backend--9mktmlp9s6kq.code.run/',
   googleMapsKey: 'AIzaSyBCMLN-ZbmFDw7Rf_OLsCDrYzI4n-jcaX0',
-  apiUrlAnexos: 'http://192.168.10.28/',
 };
