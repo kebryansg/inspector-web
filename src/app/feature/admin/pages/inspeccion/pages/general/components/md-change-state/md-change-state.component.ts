6dc32df1618b3d40560bff1745026b2c8bed9a45
KEVIN SUAREZ <kebryansg@gmail.com>
2024-07-11 22:45:29 -0500

    feat(change-state): added field dateInspection

--- src/app/feature/admin/pages/inspeccion/pages/general/components/md-change-state/md-change-state.component.ts
+++ src/app/feature/admin/pages/inspeccion/pages/general/components/md-change-state/md-change-state.component.ts
@@ -1,10 +1,14 @@
-import {ChangeDetectionStrategy, Component, inject, OnInit, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, signal} from '@angular/core';
 import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 import {ItemControlComponent} from "@standalone-shared/forms/item-control/item-control.component";
-import {DxSelectBoxModule, DxTextBoxModule} from "devextreme-angular";
+import {DxDateBoxModule, DxSelectBoxModule, DxTextBoxModule} from "devextreme-angular";
 import {DxSelectErrorControlDirective} from "@directives/select-box.directive";
 import {DxTextErrorControlDirective} from "@directives/text-box.directive";
 import {ModalTemplate} from "@modal/modal-template";
+import {toSignal} from "@angular/core/rxjs-interop";
+import {map, tap} from "rxjs/operators";
+import {filter} from "rxjs";
+import {DxDateErrorControlDirective} from "@directives/date-box.directive";
 
 @Component({
   selector: 'app-md-change-state',
@@ -16,12 +20,14 @@ import {ModalTemplate} from "@modal/modal-template";
     DxTextBoxModule,
     DxSelectErrorControlDirective,
     DxTextErrorControlDirective,
+    DxDateBoxModule,
+    DxDateErrorControlDirective,
   ],
   templateUrl: './md-change-state.component.html',
   styleUrl: './md-change-state.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MdChangeStateComponent extends ModalTemplate implements OnInit {
+export class MdChangeStateComponent extends ModalTemplate {
   private fb = inject(FormBuilder);
 
   statesForm = signal([
@@ -32,10 +38,25 @@ export class MdChangeStateComponent extends ModalTemplate implements OnInit {
   formChangeState = this.fb.nonNullable.group({
     state: ['', Validators.required],
     observation: ['', Validators.required],
+    dateInspection: [new Date()],
   });
 
-  ngOnInit() {
-  }
+  isAllowReInspection = toSignal(
+    this.stateControl.valueChanges
+      .pipe(
+        filter(Boolean),
+        map<string, boolean>(state => state === 'REP'),
+        tap(result => {
+          if (result) {
+            this.dateInspectionControl.setValidators(Validators.required);
+          } else {
+            this.dateInspectionControl.clearValidators();
+          }
+          this.dateInspectionControl.updateValueAndValidity();
+        })
+      ),
+    {initialValue: false}
+  );
 
   closeModal() {
 
@@ -43,7 +64,18 @@ export class MdChangeStateComponent extends ModalTemplate implements OnInit {
     if (this.formChangeState.invalid) return;
 
     const dataForm = this.formChangeState.getRawValue();
-    this.activeModal.close(dataForm);
+    this.activeModal.close({
+      ...dataForm,
+      isAllowReInspection: this.isAllowReInspection(),
+    });
+  }
+
+  get dateInspectionControl() {
+    return this.formChangeState.controls.dateInspection
+  }
+
+  get stateControl() {
+    return this.formChangeState.controls.state
   }
 
 }
