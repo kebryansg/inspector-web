d5d9eea832fe7b3c3276236736dc6b6c6c0a38bc
Kevin SG <kebryansg@gmail.com>
2024-12-31 20:18:55 -0500

    chore(api): config endpoint prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import {EnvironmentInt} from "../app/interfaces/environment.interface";
 
 export const environment: EnvironmentInt = {
   production: true,
-  apiUrl: 'https://p01--inspector-backend--9mktmlp9s6kq.code.run/',
+  apiUrl: 'https://api-inspect.bomberosbuenafe.com/',
   googleMapsKey: 'AIzaSyBCMLN-ZbmFDw7Rf_OLsCDrYzI4n-jcaX0',
 };
